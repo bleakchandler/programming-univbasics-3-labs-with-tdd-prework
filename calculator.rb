ff328b934cf75053c06782e52fb1665f45c78e33
Bleak Chandler <bleakchandler@gmail.com>
2021-01-15 21:54:47 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 # Add your variables here
-first_number = 4
-second_number = 3
-sum = 5
-difference = 1
-product = 
-quotient= 
+first_number = 10
+second_number = 5
+sum = 15
+difference = 5
+product = 50 
+quotient = 2
